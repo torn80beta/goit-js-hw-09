aa0aefd51f7b93a5e443dfa983da4be77c380cbb
Alex <103459735+torn80beta@users.noreply.github.com>
2023-02-18 19:27:12 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyEl = document.querySelector('body');
+const startButtonEl = document.querySelector('button[data-start]');
+const stopButtonEl = document.querySelector('button[data-stop]');
+let intID;
 
+startButtonEl.addEventListener('click', () => {
+  startButtonEl.setAttribute('disabled', '');
+  stopButtonEl.removeAttribute('disabled', '');
+  colorChanger();
+});
+
+stopButtonEl.addEventListener('click', () => {
+  startButtonEl.removeAttribute('disabled', '');
+  stopButtonEl.setAttribute('disabled', '');
+  clearInterval(intID);
+});
+
+function colorChanger() {
+  intID = setInterval(
+    () => (bodyEl.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
