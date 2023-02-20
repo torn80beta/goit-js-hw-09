a9cafe5b20eec9f1b9c61346fb87bef3e502d2b0
Alex <103459735+torn80beta@users.noreply.github.com>
2023-02-20 17:06:59 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
+const refs = {
+  delayEl: document.querySelector('input[name=delay]'),
+  stepEl: document.querySelector('input[name=step]'),
+  amountEl: document.querySelector('input[name=amount]'),
+  submitButtonEl: document.querySelector('button[type=submit]'),
+};
+
+console.log(refs.delayEl, refs.stepEl, refs.amountEl);
+
+refs.submitButtonEl.addEventListener('click', e => {
+  e.preventDefault();
+  let delay = Number(refs.delayEl.value);
+  const step = Number(refs.stepEl.value);
+  const amount = Number(refs.amountEl.value);
+  console.log(`Delay -> ${delay}, Step -> ${step}, Amount -> ${amount}`);
+
+  for (let index = 1; index <= amount; index++) {
+    setTimeout(() => {
+      console.log(`Creating ${index} Promise with ${delay} delay.`);
+      createPromise(index, step, delay);
+      delay += step;
+    }, 0);
+  }
+});
+
+function createPromise(index, step, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    const promise = new Promise(() => {
+      setTimeout(
+        () => console.log(`✅ Fulfilled promise ${index} in ${delay}ms`),
+        delay
+      );
+    });
   } else {
-    // Reject
+    const promise = new Promise(() => {
+      setTimeout(
+        () => console.log(`❌ Rejected promise ${index} in ${delay}ms`),
+        delay
+      );
+    });
   }
 }
