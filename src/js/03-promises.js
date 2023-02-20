8981d8aca39010fdcfd571ce1d7ebd319e3ac510
Alex <103459735+torn80beta@users.noreply.github.com>
2023-02-20 18:00:51 +0200

    then

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,39 +5,47 @@ const refs = {
   submitButtonEl: document.querySelector('button[type=submit]'),
 };
 
-console.log(refs.delayEl, refs.stepEl, refs.amountEl);
+// console.log(refs.delayEl, refs.stepEl, refs.amountEl);
 
 refs.submitButtonEl.addEventListener('click', e => {
   e.preventDefault();
   let delay = Number(refs.delayEl.value);
   const step = Number(refs.stepEl.value);
   const amount = Number(refs.amountEl.value);
-  console.log(`Delay -> ${delay}, Step -> ${step}, Amount -> ${amount}`);
+  // console.log(`Delay -> ${delay}, Step -> ${step}, Amount -> ${amount}`);
 
   for (let index = 1; index <= amount; index++) {
     setTimeout(() => {
-      console.log(`Creating ${index} Promise with ${delay} delay.`);
-      createPromise(index, step, delay);
+      createPromise(index, delay)
+        .then(({ index, delay }) => {
+          console.log(`✅ Fulfilled promise ${index} in ${delay}ms`);
+        })
+        .catch(({ index, delay }) => {
+          console.log(`❌ Rejected promise ${index} in ${delay}ms`);
+        });
       delay += step;
     }, 0);
   }
 });
 
-function createPromise(index, step, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    const promise = new Promise(() => {
-      setTimeout(
-        () => console.log(`✅ Fulfilled promise ${index} in ${delay}ms`),
-        delay
-      );
-    });
-  } else {
-    const promise = new Promise(() => {
-      setTimeout(
-        () => console.log(`❌ Rejected promise ${index} in ${delay}ms`),
-        delay
-      );
-    });
-  }
+function createPromise(index, delay) {
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.5;
+      if (shouldResolve) {
+        resolve({ index, delay });
+      } else {
+        reject({ index, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
+
+createPromise(2, 1500)
+  .then(({ index, delay }) => {
+    console.log(`✅ Fulfilled promise ${index} in ${delay}ms`);
+  })
+  .catch(({ index, delay }) => {
+    console.log(`❌ Rejected promise ${index} in ${delay}ms`);
+  });
