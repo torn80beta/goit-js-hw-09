0796841732c04d88911fdedd6db60d6192ae9eb6
Alex <103459735+torn80beta@users.noreply.github.com>
2023-02-20 18:15:03 +0200

    notify

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const refs = {
   delayEl: document.querySelector('input[name=delay]'),
   stepEl: document.querySelector('input[name=step]'),
@@ -5,23 +7,20 @@ const refs = {
   submitButtonEl: document.querySelector('button[type=submit]'),
 };
 
-// console.log(refs.delayEl, refs.stepEl, refs.amountEl);
-
 refs.submitButtonEl.addEventListener('click', e => {
   e.preventDefault();
   let delay = Number(refs.delayEl.value);
   const step = Number(refs.stepEl.value);
   const amount = Number(refs.amountEl.value);
-  // console.log(`Delay -> ${delay}, Step -> ${step}, Amount -> ${amount}`);
 
   for (let index = 1; index <= amount; index++) {
     setTimeout(() => {
       createPromise(index, delay)
         .then(({ index, delay }) => {
-          console.log(`✅ Fulfilled promise ${index} in ${delay}ms`);
+          Notify.success(`✅ Fulfilled promise ${index} in ${delay}ms`);
         })
         .catch(({ index, delay }) => {
-          console.log(`❌ Rejected promise ${index} in ${delay}ms`);
+          Notify.failure(`❌ Rejected promise ${index} in ${delay}ms`);
         });
       delay += step;
     }, 0);
@@ -44,8 +43,8 @@ function createPromise(index, delay) {
 
 createPromise(2, 1500)
   .then(({ index, delay }) => {
-    console.log(`✅ Fulfilled promise ${index} in ${delay}ms`);
+    Notify.success(`✅ Fulfilled promise ${index} in ${delay}ms`);
   })
   .catch(({ index, delay }) => {
-    console.log(`❌ Rejected promise ${index} in ${delay}ms`);
+    Notify.failure(`❌ Rejected promise ${index} in ${delay}ms`);
   });
